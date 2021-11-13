617a2d64e03984b1576db437e1b232c0b77cecec
yeffry <yeffry45@gmail.com>
2021-11-13 12:05:15 -0500

    complete exercises 1.6 + 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = ({onClick, text}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button onClick={onClick}>{text}</button>
+  )
 }
 
-export default App;
+const App = () => {
+  const [ good, setGood ] = useState(0)
+  const [ neutral, setNeutral ] = useState(0)
+  const [ bad, setBad ] = useState(0)
+
+  const total = good + neutral + bad
+  const average = good + neutral + bad / 3
+  const positive = (good / total)  * 100
+
+  
+  const handleSetGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleSetNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleSetBad = () => {
+    setBad(bad + 1)
+  }
+
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button onClick={handleSetGood} text='Good'/>
+      <Button onClick={handleSetNeutral} text='Neutral'/>
+      <Button onClick={handleSetBad} text='Bad'/>
+
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      
+      <p>Total: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}</p>
+    </div>
+  )
+}
+export default App
