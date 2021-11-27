877c483f03434228c471ebaee8fa059dbb671a2d
yeffry <yeffry45@gmail.com>
2021-11-26 20:30:10 -0500

    add solution for exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,16 +4,31 @@ const Person = (props) => {
   return (
     <div>
       {props.data.map((person) => 
-        <p key={person.id}>{person.name} - {person.phone}</p>
+        <p key={person.id}>{person.name} - {person.number}</p>
       )}
     </div>
   )
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
+  const [ searchTerm, setSearchTerm ] = useState('')
+
+  const personsToShow = searchTerm === ""
+    ? persons
+    : persons.filter((person) => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+  const handleFilterChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
 
   const handlePersonChange = (event) => {
     setNewName(event.target.value)
@@ -35,17 +50,19 @@ const App = () => {
 
     const personObject = {
       name: newName,
-      phone: newPhone,
+      number: newPhone,
       id: persons.length + 1
     }
     
     setPersons(persons.concat(personObject))
     setNewName('')
   }
-  console.log(persons)
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown: <input type='text' value={searchTerm} onChange={handleFilterChange}/>
+      </div>
       <form onSubmit={handleAddPerson}>
         <div> 
           Name: <input type='text' value={newName} onChange={handlePersonChange}/> 
@@ -59,7 +76,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        <Person data={persons}/>
+        {}
+        <Person data={personsToShow}/>
       </div>
     </div>
   )
