a62ad4b2c1a6f61a893463030cdcd4467c314f70
yeffry <yeffry45@gmail.com>
2021-12-21 18:32:39 -0500

    Modify initial state to be fetched from the server using the axios

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = (props) => {
   return (
@@ -11,7 +12,6 @@ const Person = (props) => {
 }
 
 const Filter = (props) => {
-  console.log(props)
   return (
     <div>
         Filter shown: <input type='text' value={props.value} onChange={props.onChange}/>
@@ -35,17 +35,20 @@ const PersonForm = (props) => {
   )
 }
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
   const [ searchTerm, setSearchTerm ] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        setPersons(response.data)
+      })
+
+  }, [])
+  
   const personsToShow = searchTerm === ""
     ? persons
     : persons.filter((person) => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
