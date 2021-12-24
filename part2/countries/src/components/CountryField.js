9bb72f7985d8bf4fd19ce6f5aa07970b3d24454b
yeffry <yeffry45@gmail.com>
2021-12-23 19:18:59 -0500

    add CountryField component

---
+++ part2/countries/src/components/CountryField.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+import CountryFilter from './CountryFilter';
+import CountryData from './CountryData';
+
+const CountryField = ({ data, setSearched }) => {
+	console.log(data);
+	if (data.length > 10) {
+		return <div>Too many matches, specify another filter</div>;
+	} else if (data.length > 1 && data.length < 10) {
+		return <CountryFilter countryInfo={data} setSearched={setSearched} />;
+	} else if (data.length === 1) {
+		return <CountryData countryInfo={data} />;
+	} else {
+		return null;
+	}
+};
+
+export default CountryField;
