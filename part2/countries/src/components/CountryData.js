96bbd6fc86643436a497afa48d2bad7c41298d7a
yeffry <yeffry45@gmail.com>
2021-12-23 19:17:54 -0500

    add CountryData component for rendering country info

--- part2/countries/src/components/CountryData.js
+++ part2/countries/src/components/CountryData.js
@@ -1,34 +1,20 @@
 import React from "react";
 import { v4 as uuidv4 } from 'uuid';
 
-const CountryData = ({ data }) => {
-  if(data.length === 1) {
-    return (
-      <React.Fragment>
-        <h1>{data[0].name.common}</h1>
-        <p>{data[0].capital[0]}</p>
-        <p>{data[0].population}</p>
-        <h3>Languages</h3>
-        <ul>
-          {Object.values(data[0].languages).map((val) => <li key={uuidv4()}>{val}</li>)}
-        </ul>
-        <img alt="flag" src={data[0].flags.png} />
-      </React.Fragment>
-    )
-  }
+const CountryData = ({ countryInfo }) => {
+  const country = countryInfo[0];
+  // console.log(countryInfo)
+  console.log(country)
   return (
     <React.Fragment>
-      {data.length > 10 
-        ? 'Too many matches, specify another filter' 
-        : data.map((country) => {
-          console.log(country)
-            return (
-              <p key={country.area}>{country.name.common}
-                <button>show</button>
-              </p>
-            )
-        })
-      }
+      <h1>{country.name.common}</h1>
+      <p>{country.capital[0]}</p>
+      <p>{country.population}</p>
+      <h3>Languages</h3>
+      <ul>
+        {Object.values(country.languages).map((val) => <li key={uuidv4()}>{val}</li>)}
+      </ul>
+      <img alt="flag" src={country.flags.png} />
     </React.Fragment>
   )
 }
