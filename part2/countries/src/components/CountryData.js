07187047d8435db7140b80ee2172ae59e4819824
yeffry <yeffry45@gmail.com>
2021-12-24 17:04:29 -0500

    add weather display capability

--- part2/countries/src/components/CountryData.js
+++ part2/countries/src/components/CountryData.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios'
+import CountryWeather from "./CountryWeather";
 
 const CountryData = ({ countryInfo }) => {
+  const [ weather, setWeather ] = useState(null)
+
   const country = countryInfo[0];
-  // console.log(countryInfo)
-  console.log(country)
+
+  useEffect(() => {
+    axios
+      .get(`http://api.openweathermap.org/data/2.5/weather?lat=${country.latlng[0]}&lon=${country.latlng[1]}&appid=${process.env.REACT_APP_API_KEY}`)
+      .then((response) => {
+        setWeather(response.data)
+      })
+  }, [country,])
+
   return (
     <React.Fragment>
       <h1>{country.name.common}</h1>
@@ -15,6 +26,7 @@ const CountryData = ({ countryInfo }) => {
         {Object.values(country.languages).map((val) => <li key={uuidv4()}>{val}</li>)}
       </ul>
       <img alt="flag" src={country.flags.png} />
+      {weather !== null ? <CountryWeather weather={weather}/> : null}
     </React.Fragment>
   )
 }
