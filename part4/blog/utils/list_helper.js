cba9fb2ac4f6828102c10875048d3517ee29d191
yeffry <yeffry45@gmail.com>
2022-05-06 13:00:35 -0400

    add solution for exercise 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, curr) => {
+    return curr.likes += acc
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
